d5396190cccac0fe81bde17a92d19f4ef9b49ba2
Ugo Proietti <ugo.pr@protonmail.com>
2021-03-05 01:54:12 +0100

    MapChecker progression

--- app/src/main/java/sokoban/Engine/Tools/MapChecker.java
+++ app/src/main/java/sokoban/Engine/Tools/MapChecker.java
@@ -1,26 +1,40 @@
 package sokoban.Engine.Tools;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class MapChecker {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(args));
-        if (args[0] == "f") {
-            System.out.println(checkFile(args[1]));
-        } else if (args[0] == "d") {
-            System.out.println(checkDirectory(args[1]));
+        if (args[0].equals("f")) {
+            checkFile(args[1]);
+        } else if (args[0].equals("d")) {
+            checkDirectory(args[1]);
+            // System.out.println(checkDirectory(args[1]));
         } else {
             System.out.println("Bad argument ! You should use 'f' for file or 'd' for directory.");
         }
     }
 
-    private static char[] checkDirectory(String string) {
-        return null;
+    private static void checkFile(String file) {
+        File myObj = new File(file);
+        Scanner myReader = new Scanner(myObj);
+        while (myReader.hasNextLine()) {
+            String currentLine = myReader.nextLine();
+
+        }
+        myReader.close();
     }
 
-    private static String checkFile(String string) {
-        return null;
+    private static void checkDirectory(String path) {
+        File folder = new File(path);
+        File[] listOfFiles = folder.listFiles();
+
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile() && listOfFiles[i].getName().endsWith(".xsb")) {
+                checkFile(listOfFiles[i].getAbsolutePath());
+            }
+        }
     }
 }
