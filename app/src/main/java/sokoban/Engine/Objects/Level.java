08ee83a067b46fce40d2380a61a74f7611bda242
Theo <210170@umons.ac.be>
2021-04-08 15:16:42 +0200

    + comments in Level.java

--- app/src/main/java/sokoban/Engine/Objects/Level.java
+++ app/src/main/java/sokoban/Engine/Objects/Level.java
@@ -10,7 +10,11 @@ public class Level {
     private World world;
     private Player player;
     private int[] size;
-    
+  
+    /**
+     * This create an instance of a level.
+     * @param map is a string containing a map name. ex: map1 
+     */
     public Level(String map) {
         int[] pos = {0, 0};
         this.map = MapLoader.load("build/resources/main/levels/"+map+".xsb"); 
@@ -18,19 +22,27 @@ public class Level {
         player = new Player(pos, "/Cells/player.png");
         world = new World(size[0], size[1], player);
         Builder.init(this.map, player, world, size[0], size[1]);
-
     }
 
+    /**
+     * By default, a level is locked but it can be 
+     * unlock by using this method.
+     */
     public void unlock() {
         locked = false;
     }
 
+    /**
+     * This return the world of the current instance of a level.
+     */
     public World getWorld() {
         return world;
     }
 
+    /** 
+     * This return the player of the current isntance of a level.
+     */
     public Player getPlayer() {
         return player;
-        }
+    }
 }
-
