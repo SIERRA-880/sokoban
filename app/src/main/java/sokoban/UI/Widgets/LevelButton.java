dc7a58a5a64f67dc4e9a8fd480f415421d2e5f39
Harbiinger <theogodin51@gmail.com>
2021-04-09 15:46:53 +0200

    using Level for UI and new textures

--- app/src/main/java/sokoban/UI/Widgets/LevelButton.java
+++ app/src/main/java/sokoban/UI/Widgets/LevelButton.java
@@ -5,6 +5,7 @@ import sokoban.Engine.Objects.Player;
 import sokoban.Engine.Objects.World;
 import sokoban.Engine.Tools.Builder;
 import sokoban.Engine.Tools.MapLoader;
+import sokoban.Engine.Objects.Level;
 
 public class LevelButton extends ImageButton {
     //type of button that is used to designate a level
@@ -20,12 +21,9 @@ public class LevelButton extends ImageButton {
 
     public Map getMap() {
         try {
-            String map = MapLoader.load("build/resources/main/levels/map" + (level) + ".xsb");
-            int[] size = MapLoader.getSize("build/resources/main/levels/map" + (level) + ".xsb");
-            int[] pos = {0, 0};
-            player = new Player(pos, "/Cells/player.png");
-            world = new World(size[0], size[1], player);
-            Builder.init(map, player, world, size[0], size[1]);
+            Level map = new Level("map"+level);
+            world = map.getWorld();
+            player = map.getPlayer();
         } catch (Exception e) {
             e.printStackTrace();
         }
