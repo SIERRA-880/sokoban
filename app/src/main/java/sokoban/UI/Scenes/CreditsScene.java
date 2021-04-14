d640da5e2584a0d03cc8b119f027e73e827ea710
Harbiinger <theogodin51@gmail.com>
2021-04-14 12:10:51 +0200

    new fonts for CreditsScene

--- app/src/main/java/sokoban/UI/Scenes/CreditsScene.java
+++ app/src/main/java/sokoban/UI/Scenes/CreditsScene.java
@@ -3,6 +3,7 @@ package sokoban.UI.Scenes;
 import javafx.geometry.Pos;
 import javafx.geometry.Insets;
 
+import javafx.scene.text.Font;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
@@ -38,6 +39,14 @@ public class CreditsScene extends Scene {
         }
 
         // credits 
+        Font f = null;
+        try {
+            f = Font.loadFont(new FileInputStream(new File("build/resources/main/textures/Default/Fonts/Kenney Rocket Square.ttf")), 20);
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("Fonts file not found (CreditsScene:43)");
+        }
         
         // gridPane
         GridPane grid = new GridPane();
@@ -47,53 +56,53 @@ public class CreditsScene extends Scene {
 
         //labels 
         Label l1 = new Label("Developers");
+        l1.setFont(f);
         l1.setTextFill(Color.web("#FFFFFF"));
-        l1.setStyle("-fx-font: 24 arial;");
         grid.add(l1, 0, 0);
 
         Label l2 = new Label("Sierra-880, Tiramisu, Harbinger");
+        l2.setFont(f);
         l2.setTextFill(Color.web("#FFFFFF"));
-        l2.setStyle("-fx-font: 24 arial;");
         grid.add(l2, 1, 0);
 
         Label l3 = new Label("BackGround Video in the main menu");
+        l3.setFont(f);
         l3.setTextFill(Color.web("#FFFFFF"));
-        l3.setStyle("-fx-font: 24 arial;");
         grid.add(l3, 0, 1);
 
         Label l4 = new Label("https://www.youtube.com/watch?v=B02Q3cg2wtY");
+        l4.setFont(f);
         l4.setTextFill(Color.web("#FFFFFF"));
-        l4.setStyle("-fx-font: 24 arial;");
         grid.add(l4, 1, 1);
 
         Label l5 = new Label("Music in the main menu");
+        l5.setFont(f);
         l5.setTextFill(Color.web("#FFFFFF"));
-        l5.setStyle("-fx-font: 24 arial;");
         grid.add(l5, 0, 2);
 
         Label l6 = new Label("https://www.youtube.com/watch?v=L5pfhWye3UM");
+        l6.setFont(f);
         l6.setTextFill(Color.web("#FFFFFF"));
-        l6.setStyle("-fx-font: 24 arial;");
         grid.add(l6, 1, 2);
 
         Label l7 = new Label("Sounds in the levels");
+        l7.setFont(f);
         l7.setTextFill(Color.web("#FFFFFF"));
-        l7.setStyle("-fx-font: 24 arial;");
         grid.add(l7, 0, 3);
 
         Label l8 = new Label("https://opengameart.org/content/9-sci-fi-computer-sounds-and-beeps");
+        l8.setFont(f);
         l8.setTextFill(Color.web("#FFFFFF"));
-        l8.setStyle("-fx-font: 24 arial;");
         grid.add(l8, 1, 3);
 
         Label l9 = new Label("Boxes' texture in the levels");
+        l9.setFont(f);
         l9.setTextFill(Color.web("#FFFFFF"));
-        l9.setStyle("-fx-font: 24 arial;");
         grid.add(l9, 0, 4);
 
         Label l10 = new Label("https://opengameart.org/content/top-down-2d-metal-box");
+        l10.setFont(f);
         l10.setTextFill(Color.web("#FFFFFF"));
-        l10.setStyle("-fx-font: 24 arial;");
         grid.add(l10, 1, 4);
 
         stackPane.getChildren().add(grid);
