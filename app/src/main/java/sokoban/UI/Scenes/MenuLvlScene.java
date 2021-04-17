8a25e239ea458d5664b401463c191bcc503ec100
Harbiinger <theogodin51@gmail.com>
2021-04-17 09:01:09 +0200

    label in menuLvlScene

--- app/src/main/java/sokoban/UI/Scenes/MenuLvlScene.java
+++ app/src/main/java/sokoban/UI/Scenes/MenuLvlScene.java
@@ -7,6 +7,9 @@ import javafx.scene.ImageCursor;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 import sokoban.Game;
 import sokoban.UI.Widgets.BackButton;
@@ -50,5 +53,21 @@ public class MenuLvlScene extends Scene {
         stackPane.getChildren().add(bbutton);
         StackPane.setAlignment(bbutton, Pos.TOP_LEFT);
         StackPane.setMargin(bbutton, new Insets(20.0, 0.0, 0.0, 20.0));
+
+        // label
+        Font f = null;
+        try {
+            f = Font.loadFont(new FileInputStream("build/resources/main/textures/Default/Fonts/Kenney Rocket Square.ttf"), 40);
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("Fonts file not found (MenuLvlScene:44)");
+        }
+        Label selectLabel = new Label("Select a level :");
+        selectLabel.setFont(f);
+        selectLabel.setTextFill(Color.web("#A7F5F4"));
+        stackPane.getChildren().add(selectLabel);
+        stackPane.setAlignment(selectLabel, Pos.TOP_CENTER);
+        stackPane.setMargin(selectLabel, new Insets(160.0, 0.0, 0.0, 0.0));
     }
 }
