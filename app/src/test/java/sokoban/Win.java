9ca4aa0fa650aadab451935aad649ddfea7e2001
Ugo Proietti <ugo.pr@protonmail.com>
2021-04-21 13:17:14 +0200

    Adding unit test

--- app/src/test/java/sokoban/Win.java
+++ app/src/test/java/sokoban/Win.java
@@ -6,10 +6,18 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class Win {
-    @Test public void win() {
+    @Test public void win1() {
         Level level = new Level();
         level.setLevel("test1");
         level.player.move("right", level.world);
+        assertEquals(true,level.world.winCondition());
+    }
+
+    @Test public void win2() {
+        Level level = new Level();
+        level.setLevel("test2");
+        level.player.move("down", level.world);
+        level.player.move("left", level.world);
         assertEquals(true,level.world.winCondition());
     }
 }
